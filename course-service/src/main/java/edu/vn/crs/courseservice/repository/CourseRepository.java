392646c93901f10df7f3dd56e367fc5d0266918a
tnnhuaa031204 <tnnh.uaa04@gmail.com>
2026-08-13 10:49:06 +0700

    feat: jwt verification + role-based authorization across services

--- course-service/src/main/java/edu/vn/crs/courseservice/repository/CourseRepository.java
+++ course-service/src/main/java/edu/vn/crs/courseservice/repository/CourseRepository.java
@@ -1,6 +1,7 @@
 package edu.vn.crs.courseservice.repository;
 
 import edu.vn.crs.courseservice.entity.Course;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
